21e7bcaaf2288d80e9c962c365b2db268f396d55
NecroBread <mariovyordanov@gmail.com>
2023-12-19 12:49:11 +0200

    add routes and about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { NotesComponent } from "./notes/notes.component";
+import { AboutComponent } from "./about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: NotesComponent },
+  { path: "about", component: AboutComponent },
+  { path: "**", redirectTo: "/about" },
+];
--- src/app/notes/components/notes-list/notes-list.component.ts
+++ src/app/notes/components/notes-list/notes-list.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from "@angular/core";
 import { NoteCardComponent } from "../note-card/note-card.component";
 import { NoteEntity } from "../../../shared/types/note";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-notes-list",
   standalone: true,
-  imports: [NoteCardComponent],
+  imports: [NoteCardComponent, RouterLink],
   templateUrl: "./notes-list.component.html",
   styleUrl: "./notes-list.component.css",
   changeDetection: ChangeDetectionStrategy.OnPush,
