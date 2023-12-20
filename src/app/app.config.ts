f913c6f9d870bf38466b43ac49abc0a8c4bddca0
NecroBread <mariovyordanov@gmail.com>
2023-12-20 09:59:41 +0200

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideQuillConfig } from "ngx-quill";
 import { QuillConfiguration } from "./shared/configs/quill.config";
+import { provideServiceWorker } from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,10 @@ export const appConfig: ApplicationConfig = {
       modules: {
         toolbar: QuillConfiguration.toolbar,
       },
+    }),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
     }),
   ],
 };
